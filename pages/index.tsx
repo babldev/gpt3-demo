910573bac3393186d0f913363808c6f5f4ac645a
Brady Law <bradyl16@gmail.com>
2022-08-24 14:59:04 -0700

    Add some buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,58 @@
-import { AppShell, Header, Navbar } from '@mantine/core'
+import { AppShell, Group, Header, Navbar, Text, ThemeIcon, UnstyledButton } from '@mantine/core'
+import { IconAlertCircle, IconDatabase, IconGitPullRequest, IconMessages } from '@tabler/icons';
 import type { NextPage } from 'next'
+
+interface MainLinkProps {
+  icon: React.ReactNode;
+  color: string;
+  label: string;
+}
+
+function MainLink({ icon, color, label }: MainLinkProps) {
+  return (
+    <UnstyledButton
+      sx={(theme) => ({
+        display: 'block',
+        width: '100%',
+        padding: 6,
+        borderRadius: theme.radius.sm,
+        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+        '&:hover': {
+          backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Group>
+        <ThemeIcon color={color} variant="light" size="sm">
+          {icon}
+        </ThemeIcon>
+
+        <Text size="xs">{label}</Text>
+      </Group>
+    </UnstyledButton>
+  );
+}
+
+const data = [
+  { icon: <IconGitPullRequest size={16} />, color: 'blue', label: 'Pull Requests' },
+  { icon: <IconAlertCircle size={16} />, color: 'teal', label: 'Open Issues' },
+  { icon: <IconMessages size={16} />, color: 'violet', label: 'Discussions' },
+  { icon: <IconDatabase size={16} />, color: 'grape', label: 'Databases' },
+];
+
+export function MainLinks() {
+  const links = data.map((link) => <MainLink {...link} key={link.label} />);
+  return <div>{links}</div>;
+}
 
 const Home: NextPage = () => {
   return <AppShell
     padding="md"
-    navbar={<Navbar width={{ base: 250 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+    navbar={<Navbar width={{ base: 250 }} height="100%" p={6}>
+      <MainLinks />
+    </Navbar>}
     header={<Header height={40} p="xs">{/* Header content */}</Header>}
     styles={(theme) => ({
       main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
